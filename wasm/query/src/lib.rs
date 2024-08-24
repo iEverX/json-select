be01179658c9ed72a091e69a69da859386ed1f3d
Ever <g.ever1991@gmail.com>
2024-08-24 21:11:59 +0800

    fix select logic

--- wasm/query/src/lib.rs
+++ wasm/query/src/lib.rs
@@ -1,43 +1,87 @@
 use std::thread::sleep;
-use anyhow;
-use serde_json::{Value, Map};
+use anyhow::{anyhow, Error};
+use serde_json::{Value, Map, json, Number};
+use serde_json::Value::Object;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
 
 pub fn query_str(full: &str, select: &str) -> anyhow::Result<String> {
-    let full_json: Value = serde_json::from_str(full)?;
-    let select_json: Value = serde_json::from_str(select)?;
-    let value = do_query(&full_json, &select_json);
-    Ok(serde_json::to_string_pretty(&value)?)
+    let config = QueryConfig { pretty: true };
+    query_str_with_config(full, select, config)
 }
 
-pub fn do_query(full: &Value, select: &Value) -> Value {
-    let full_clone = select.is_boolean() && select.as_bool().unwrap()
-        || select.is_i64() && select.as_i64().unwrap() > 0;
+struct QueryConfig {
+    pretty: bool,
+}
 
-    if full_clone {
-        full.clone()
-    } else if full.is_array() {
-        let mut arr = Vec::<Value>::new();
-        for value in full.as_array().unwrap() {
-            let r = do_query(value, select);
-            arr.push(r);
-        }
-        Value::Array(arr)
+fn query_str_with_config(full: &str, select: &str, query_config: QueryConfig) -> anyhow::Result<String> {
+    let full_json: Value = serde_json::from_str(full)?;
+    let select_json  = serde_json::from_str(select)?;
+    let value = do_query(&full_json, &select_json)?;
+
+    if query_config.pretty {
+        Ok(serde_json::to_string_pretty(&value)?)
     } else {
-        let mut m = Map::<String, Value>::new();
+        Ok(serde_json::to_string(&value)?)
+    }
+}
 
-        let kvs = select.as_object().unwrap();
-        for (k, v) in kvs {
-            if let Some(value) = full.get(k) {
-                m.insert(k.clone(), do_query(value, v));
+fn do_query(full: &Value, select: &Value) -> anyhow::Result<Option<Value>> {
+    let result = match select {
+        Value::Bool(x) => {
+            if *x {
+                Ok(Some(full.clone()))
+            } else {
+                Ok(None)
             }
         }
-        Value::Object(m)
-    }
+        Value::Number(x) => {
+            if x.is_i64() && x.as_i64().unwrap() > 0 {
+                Ok(Some(full.clone()))
+            } else {
+                Ok(None)
+            }
+        }
+        Object(x) => {
+            if full.is_array() {
+                let mut arr = Vec::<Value>::new();
+                for value in full.as_array().unwrap() {
+                    if let Some(r) = do_query(value, select)? {
+                        arr.push(r);
+                    }
+                }
+                Ok(Some(Value::Array(arr)))
+            } else if full.is_object() {
+                let mut m = Map::<String, Value>::new();
+
+                let kvs = select.as_object().unwrap();
+                for (k, v) in kvs {
+                    if let Some(value) = full.get(k) {
+                        if let Some(r) = do_query(value, v)? {
+                            m.insert(k.clone(), r);
+                        }
+                    }
+                }
+                Ok(Some(Object(m)))
+            } else {
+                Err(anyhow!("object select with non-object json field"))
+            }
+        }
+        Value::Null => {
+            Err(anyhow!("null select"))
+        }
+        Value::String(x) => {
+            Err(anyhow!("string select {}", x))
+        }
+        Value::Array(_) => {
+            Err(anyhow!("array select"))
+        }
+    };
+    result
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
     #[test]
     fn query_str_simple() {
@@ -67,12 +111,12 @@ mod tests {
 
         let result = query_str(full, query);
         if result.is_err() {
-           eprintln!("{:?}", result.err()) ;
+            eprintln!("{:?}", result.err());
             return;
         }
         assert!(result.is_ok());
 
-    let expected = r#"{
+        let expected = r#"{
   "cities": [
     {
       "name": "AA"
@@ -85,6 +129,55 @@ mod tests {
 }"#;
 
         assert_eq!(result.unwrap(), expected);
+    }
 
+    #[test]
+    fn query_str_with_zero() {
+        let full = r#"
+        {
+            "cities": [
+                {
+                    "name": "AA",
+                    "code": 1
+                },
+                {
+                    "name": "BB",
+                    "code": 2
+                }
+            ],
+            "data": "content",
+            "value": {
+                "a": 1,
+                "b": 2
+            }
+        }"#;
+
+        let query = r#"
+        {
+            "cities": {
+                "name": 0
+            },
+            "value": 1
+        }"#;
+
+        let result = query_str(full, query);
+        if result.is_err() {
+            eprintln!("====================== {:?}", result.err());
+            return;
+        }
+        assert!(result.is_ok());
+
+        let expected = r#"{
+  "cities": [
+    {},
+    {}
+  ],
+  "value": {
+    "a": 1,
+    "b": 2
+  }
+}"#;
+
+        assert_eq!(result.unwrap(), expected);
     }
 }
